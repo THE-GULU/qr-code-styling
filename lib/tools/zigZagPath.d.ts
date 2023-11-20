484c585389d62eb50e886b9ac195862c30c11e60
KAZE LI <kaze.li@thegulu.com>
2023-11-20 18:12:26 +0800

    add lib

---
+++ lib/tools/zigZagPath.d.ts
@@ -0,0 +1,2 @@
+export declare const getZigZagPath: (triangleHeight: number, triangleBase: number, rect: any, axis: string, { offset, ending, shift }: any, ctx: any) => void;
+export declare const getZigZagRectPath: (triangleHeight: number, triangleBase: number, rect: any, axis: string, { offset, ending, shift, clipStart, clipEnd, stroke }: any, ctx: any) => null;
