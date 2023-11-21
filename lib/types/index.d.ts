ce349a0baca7e69f0a5c7a0d4d6e0989874c7d9c
KAZE LI <kaze.li@thegulu.com>
2023-11-21 09:56:42 +0800

    update build lib

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -3,7 +3,7 @@ export interface UnknownObject {
 }
 export type DotType = "dots" | "rounded" | "classy" | "classy-rounded" | "square" | "extra-rounded";
 export type CornerDotType = "dot" | "square";
-export type CornerSquareType = "dot" | "square" | "extra-rounded";
+export type CornerSquareType = "dot" | "square" | "extra-rounded" | "zig-zag";
 export type Extension = "svg" | "png" | "jpeg" | "webp";
 export type GradientType = "radial" | "linear";
 export type DrawType = "canvas" | "svg";
